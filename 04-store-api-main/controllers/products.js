a4691e73739e8f528b99d33632a2bf4925836bb0
Gabby <murkymarshmallow@gmail.com>
2026-08-20 10:55:41 -0600

    lunch
    saving before lunch

--- 04-store-api-main/controllers/products.js
+++ 04-store-api-main/controllers/products.js
@@ -1,9 +1,43 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
+	const products = (await Product.find({})).select("name price");
 	res.status(200).json({ msg: "products testing route" });
 };
 
 const getAllProducts = async (req, res) => {
-	res.status(200).json({ msg: "products route" });
+	const { featured, company, name, sort, fields } = req.query;
+	const queryObject = {};
+
+	if (featured) {
+		queryObject.featured = featured === "true" ? true : false;
+	}
+	if (company) {
+		queryObject.company = company;
+	}
+	if (name) {
+		queryObject.name = { $regex: name, $options: "i" };
+	}
+
+	//console.log(queryObject);
+	let result = Product.find(queryObject);
+	// sort
+	if (sort) {
+		const sortList = sort.split(",").join(" ");
+		result = result.sort(sortList);
+	} else {
+		result = result.sort("createdAt");
+	}
+
+	if (fields) {
+		const fieldsList = fields.split(",").join(" ");
+		result = result.select(fieldsList);
+	}
+
+	const page = Number(req.query.page) || 1;
+	const limit = Number(req.query.limit) || 10;
+	const products = await result;
+	res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
